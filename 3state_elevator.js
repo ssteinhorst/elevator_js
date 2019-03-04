9167a7868157e55cdd07d1fe0bf47911246fc933
Scott Steinhorst <scott.steinhorst@gmail.com>
2019-03-03 17:12:46 -0700

    code cleanup

--- 3state_elevator.js
+++ 3state_elevator.js
@@ -12,73 +12,14 @@ class ElevatorShaft {
     }
 
     move() {
-        let self = this;
-        // const totalStates = this.states.length;
-        // console.log("current floor: "+this.currentFloor);
-        // let currentState = this.states.find(action => action === this.current);
-        // this bit of commented code just runs it between the states, we need to be smarter
-        // if (currentState + 1 < totalStates) this.current = this.states[currentState + 1];
-        // else this.current = this.states[0];
-        // console.log(currentState);
-
-
-        // think OOP. what does the state know about what to do? what is the elevator doing
-        // that it can offload to the state? new floor?
-        // set current floor, prune the call list
-        let up = this.current.pruneUpCalls();
-        let down = this.current.pruneDownCalls();
-
         this.currentFloor = this.current.move();
         this.upCalls = this.current.pruneUpCalls();
         this.downCalls = this.current.pruneDownCalls();
         if(this.current.shouldChangeState()) {
             this.current = this.states[this.current.getChangedState()];
-            // this.current = new MovingDown(self);
         }
-        // this.upCalls = this.current.pruneUpCalls();
-        // this.downCalls = this.current.pruneDownCalls();
-        // if(this.current.direction() === "UP" ) {
-        //     // remove current floor from call list
-        //     var index = this.upCalls.indexOf(this.currentFloor);
-        //     if(index ==! -1) {
-        //         this.upCalls.splice(index, 1);
-        //     }
-        //
-        //
-        //
-        //     if(this.upCalls.findIndex( floor => floor > this.currentFloor) != -1) { // move up, keep moving
-        //         this.currentFloor++;
-        //     } else if (this.downCalls.findIndex( floor => floor < this.currentFloor) != -1) { // move down, change state
-        //         this.currentFloor--;
-        //         this.current = this.states[2];
-        //     } else { // no more calls to process, go idle
-        //         this.current = this.states[0];
-        //     }
-        //
-        // } else if(this.current.direction() === "DOWN") {
-        //     // remove current floor from call list
-        //     var index = this.downCalls.indexOf(this.currentFloor);
-        //     if(index ==! -1) {
-        //         this.downCalls.splice(index, 1);
-        //     }
-        //
-        //
-        //     if(this.downCalls.findIndex( floor => floor < this.currentFloor) != -1) {// move down, kep moving
-        //         this.currentFloor--;
-        //     } else if(this.upCalls.findIndex( floor => floor > this.currentFloor) != -1) { // move up, change state
-        //         this.currentFloor++;
-        //         this.current = this.states[1];
-        //     }
-        // } else if(this.current.direction() === "IDLE") {
-        //     if(this.upCalls.findIndex( floor => floor > this.currentFloor) != -1) {
-        //         this.current = this.states[1];
-        //     } else if(this.downCalls.findIndex( floor => floor < this.currentFloor) != -1) {
-        //         this.current = this.states[2];
-        //     }
-        // }
-
     }
-    // should direction be the new move()?
+
     direction() {
         return this.current.direction();
     }
@@ -111,13 +52,11 @@ class MovingUp extends Elevator {
     shouldChangeState() {
         return this.elevator.upCalls.findIndex(floor => floor > this.elevator.currentFloor) === -1;
     }
-    getChangedState(self) {
+    getChangedState() {
         if (this.elevator.downCalls.findIndex( floor => floor < this.elevator.currentFloor) !== -1) {
             return this.elevator.downIndex;
-            // return new MovingDown(self);
         } else {
-            // return this.elevator.idleIndex;
-            return new Idle(self);
+            return this.elevator.idleIndex;
         }
     }
     direction() {
